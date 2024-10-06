60f7856578f5a683a98ed5d0532bab947a197342
Pontus Lindman <pontus.v.lindman@gmail.com>
2024-10-06 10:20:33 +0300

    Added create folders

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Init common things for the experiments 
 docker network create experiments
+
+# Create common folders
+mkdir -p "$SCRIPT_DIR/services"
+mkdir -p "$SCRIPT_DIR/experiments"
